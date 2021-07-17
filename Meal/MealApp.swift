608f0e1044bcedad1ec832662f32166a62cc0c2f
Inpyo Hong <y2kpaulh@epiens.com>
2021-07-17 21:44:13 +0900

    URL parse error

--- Meal/MealApp.swift
+++ Meal/MealApp.swift
@@ -6,11 +6,29 @@
 //
 
 import SwiftUI
+import Combine
 
 @main
 struct MealApp: App {
     @StateObject var bibleStore = BibleStore()
     @StateObject var planStore = PlanStore()
+    
+    init() {
+        let url = URL(string: "https://api.jsonbin.io/b/60f298aa0cd33f7437ca62e2/1")!
+
+        let subscription = URLSession.shared
+            .dataTaskPublisher(for: url)
+            .map(\.data)
+            .decode(type: Meal.self, decoder: JSONDecoder())
+            .sink(receiveCompletion: { completion in
+                if case .failure(let err) = completion {
+                    print("Retrieving data failed with error \(err)")
+                }
+            }, receiveValue: { object in
+                print("Retrieved object \(object)")
+            })
+
+    }
     
     var body: some Scene {
         WindowGroup {
