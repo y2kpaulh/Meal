26f38458adb742dcad93401fb393875b8b1eb311
Inpyo Hong <y2kpaulh@epiens.com>
2021-08-11 19:00:58 +0900

    워치앱 메인 메뉴 수정

--- MealWatch Extension/ContentView.swift
+++ MealWatch Extension/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
 
   var body: some View {
     NavigationView {
-      VStack {
+      VStack(spacing: -4) {
         MealTitleLabel(size: 60, textColor: .white)
 
         List {
