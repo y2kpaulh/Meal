4dbe8d1c15e161d8372ec8f15c084a6c8a261747
Inpyo Hong <y2kpaulh@gmail.com>
2022-03-01 19:25:16 +0900

    끼니 일정표 제목 표시 수정

--- Meal/View/MealPlanList.swift
+++ Meal/View/MealPlanList.swift
@@ -20,31 +20,31 @@ struct MealPlanList: View {
   var body: some View {
 
     VStack {
-      //            ZStack(alignment: .center) {
-      //                Text("끼니 일정표")
-      //                    .font(.custom("NanumBrushOTF", size: 40))
-      //                    .foregroundColor(Color(UIColor.label))
-      //                    .padding(.top, 20)
-      //
-      //                HStack {
-      //                    Spacer()
-      //                    Button(
-      //                        action: {
-      //                            //store.fetchContents()
-      //                            presentationMode.wrappedValue.dismiss()
-      //                            // swiftlint:disable:next multiple_closures_with_trailing_closure
-      //                        }) {
-      //                            Image(systemName: "xmark")
-      //                                .font(.title3)
-      //                                .foregroundColor(Color(UIColor.label))
-      //                                .padding()
-      //                                .background(
-      //                                    Circle().fill((Color.closeBkgd))
-      //                                )
-      //                        }
-      //                        .padding([.top, .trailing])
-      //                }
-      //            }
+      ZStack(alignment: .center) {
+        Text("끼니 일정표")
+          .font(.custom("NanumBrushOTF", size: 30))
+          .foregroundColor(Color(UIColor.label))
+        //          .padding(.top, 20)
+
+        //                      HStack {
+        //                          Spacer()
+        //                          Button(
+        //                              action: {
+        //                                  //store.fetchContents()
+        //                                  presentationMode.wrappedValue.dismiss()
+        //                                  // swiftlint:disable:next multiple_closures_with_trailing_closure
+        //                              }) {
+        //                                  Image(systemName: "xmark")
+        //                                      .font(.title3)
+        //                                      .foregroundColor(Color(UIColor.label))
+        //                                      .padding()
+        //                                      .background(
+        //                                          Circle().fill((Color.closeBkgd))
+        //                                      )
+        //                              }
+        //                              .padding([.top, .trailing])
+        //                      }
+      }
       ScrollViewReader { scrollView in
         ScrollView {
           LazyVStack {
