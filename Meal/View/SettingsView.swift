be356a5c91447e49d0f96e20730a553cac6ab70f
Inpyo Hong <iphong@batonsos.co.kr>
2022-08-27 14:03:39 +0900

    설정 메뉴 Navigation View 적용

--- Meal/View/SettingsView.swift
+++ Meal/View/SettingsView.swift
@@ -18,59 +18,61 @@ struct SettingsView: View {
   @State private var isToggleOn: Bool = UserDefaults.standard.bool(forKey: "isDailyNoti")
 
   var body: some View {
-    Form {
-      Section(header: Text("버전 정보"), content: {
-        VStack(alignment: .leading) {
-          Text(version)
+    NavigationView {
+      Form {
+        Section(header: Text("버전 정보"), content: {
+          VStack(alignment: .leading) {
+            Text(version)
+          }
+        })
+
+        Section(header: Text("알림 설정")) {
+
+          Toggle("사용", isOn: $isToggleOn)
+            .onChange(of: isToggleOn) { _ in
+              Swift.print("isToggleOn", isToggleOn)
+              AppSettings[.isDailyNoti] = isToggleOn
+              Swift.print("AppSettings.boolValue(.isDailyNoti)", AppSettings.boolValue(.isDailyNoti))
+
+              if isToggleOn {
+                AppSettings[.dailyNotiTime] = AppSettingsManager.dailyNotiSettingsTimeFormatter.string(from: dailyNotiTime)
+                PlanStore().registDailyPush()
+              } else {
+                PlanStore().clearDailyPush()
+              }
+            }
+
+          if isToggleOn {
+            DatePicker("매일 알림 시간",
+                       selection: $dailyNotiTime,
+                       displayedComponents: .hourAndMinute)
+              .onChange(of: dailyNotiTime, perform: { _ in
+                AppSettings[.dailyNotiTime] = AppSettingsManager.dailyNotiSettingsTimeFormatter.string(from: dailyNotiTime)
+                PlanStore().registDailyPush()
+              })
+              .environment(\.locale, Locale(identifier: "ko"))
+          }
         }
-      })
 
-      Section(header: Text("알림 설정")) {
-
-        Toggle("사용", isOn: $isToggleOn)
-          .onChange(of: isToggleOn) { _ in
-            Swift.print("isToggleOn", isToggleOn)
-            AppSettings[.isDailyNoti] = isToggleOn
-            Swift.print("AppSettings.boolValue(.isDailyNoti)", AppSettings.boolValue(.isDailyNoti))
-
-            if isToggleOn {
-              AppSettings[.dailyNotiTime] = AppSettingsManager.dailyNotiSettingsTimeFormatter.string(from: dailyNotiTime)
-              PlanStore().registDailyPush()
-            } else {
-              PlanStore().clearDailyPush()
+        Section(header: Text("피드백"), content: {
+          VStack(alignment: .leading) {
+            Button(AppSettingsManager.email) {
+              let mailtoUrl = URL(string: mailtoString!)!
+              if UIApplication.shared.canOpenURL(mailtoUrl) {
+                UIApplication.shared.open(mailtoUrl, options: [:])
+              }
             }
           }
+        })
 
-        if isToggleOn {
-          DatePicker("매일 알림 시간",
-                     selection: $dailyNotiTime,
-                     displayedComponents: .hourAndMinute)
-            .onChange(of: dailyNotiTime, perform: { _ in
-              AppSettings[.dailyNotiTime] = AppSettingsManager.dailyNotiSettingsTimeFormatter.string(from: dailyNotiTime)
-              PlanStore().registDailyPush()
-            })
-            .environment(\.locale, Locale(identifier: "ko"))
-        }
+        Section(header: Text("저작권"), content: {
+          VStack(alignment: .leading) {
+            Text("개역개정판 성경 번역본은 대한성서공회의 허락을 받고 사용하였습니다.")
+          }
+          .frame(height: 80)
+        })
       }
-
-      Section(header: Text("피드백"), content: {
-        VStack(alignment: .leading) {
-          Button(AppSettingsManager.email) {
-            let mailtoUrl = URL(string: mailtoString!)!
-            if UIApplication.shared.canOpenURL(mailtoUrl) {
-              UIApplication.shared.open(mailtoUrl, options: [:])
-            }
-          }
-        }
-      })
-
-      Section(header: Text("저작권"), content: {
-        VStack(alignment: .leading) {
-          Text("개역개정판 성경 번역본은 대한성서공회의 허락을 받고 사용하였습니다.")
-        }
-        .frame(height: 80)
-      })
-
+      .navigationBarHidden(true)
     }
     .background(Color(UIColor.systemBackground))
     .frame(height: 400)
